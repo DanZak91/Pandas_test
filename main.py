ffb3d297bae8ca2b8c07037714d8cc29b2589bbe
Данилка <danil1991z@gmail.com>
2025-05-13 14:39:44 +0500

    + pandas

--- main.py
+++ main.py
@@ -328,3 +328,108 @@ df_outliers = pd.DataFrame({
 df_no_outliers = df_outliers[df_outliers['A'] < 100]
 print("\nУдаление выбросов:")
 print(df_no_outliers)
+
+
+
+
+
+#Группировка данных в DataFrame
+# Пример DataFrame
+df = pd.DataFrame({
+    'Category': ['A', 'B', 'A', 'B', 'C', 'A'],
+    'Values': [10, 20, 15, 25, 10, 30]
+})
+
+# Группировка по столбцу 'Category' и вычисление суммы значений в каждой группе
+grouped_df = df.groupby('Category', as_index=False).sum()
+print("Группировка DataFrame по столбцу 'Category' с суммированием:")
+print(grouped_df)
+
+#Есть удобный способ группировки сразу с присвоением имен колонкам:
+grouped_df2 = df.groupby('Category').agg(
+    category_sum=('Values', 'sum'),
+    category_mean=('Values', 'mean'),
+    category_count=('Values', 'count')
+)
+print('\nгруппировки сразу с присвоением имен колонкам')
+print(grouped_df2)
+
+
+
+
+
+
+#Группировка данных в Series
+# Пример Series
+s = pd.Series([10, 20, 15, 25, 10, 30], index=['A', 'B', 'A', 'B', 'C', 'A'])
+
+# Группировка по индексу и вычисление суммы значений в каждой группе/ level=0 в groupby() означает группировку по первому уровню индекса. Если индекс не многоуровневый, то это эквивалентно обычной группировке по индексу.
+grouped_s = s.groupby(level=0).sum()
+print("\nГруппировка Series по индексу с суммированием:")
+print(grouped_s)
+
+
+
+
+#АГРЕГАЦИЯ данных в DATAFRAME
+df = pd.DataFrame({
+    'Category': ['A', 'B', 'A', 'B', 'C', 'A'],
+    'Values': [10, 20, 15, 25, 10, 30]
+})
+
+# Агрегация данных: вычисление суммы и среднего значений в каждой группе
+agg_df = df.groupby('Category').agg({'Values': ['sum', 'mean']})
+print("\nАгрегация DataFrame по столбцу 'Category' с суммой и средним:")
+print(agg_df)
+
+grouped_mode = df.groupby('Category')['Values'].apply(lambda x: x.mode())
+print("\nMode (мода) для каждой категории:")
+print(grouped_mode)
+
+grouped_quantile = df.groupby('Category')['Values'].quantile(0.5)
+print("\nМедиана (50-й процентиль) для каждой категории:")
+print(grouped_quantile)
+
+
+
+
+
+#АГРЕГАЦИЯ данных в SERIES
+s = pd.Series([10, 20, 15, 25, 10, 30], index=['A', 'B', 'A', 'B', 'C', 'A'])
+
+# Агрегация данных: вычисление суммы и среднего значений в каждой группе
+agg_s = s.groupby(level=0).agg(['sum', 'mean'])
+print("\nАгрегация Series по индексу с суммой и средним:")
+print(agg_s)
+
+
+
+
+
+#ОБЪЕДИНЕНИЕ Series
+# Создание двух Series для примера
+s1 = pd.Series([1, 2, 3], index=['A', 'B', 'C'])
+s2 = pd.Series([4, 5, 6], index=['A', 'B', 'D'])
+
+# Объединение Series по строкам (по умолчанию axis=0)
+concat_s = pd.concat([s1, s2])
+print("\nОбъединение Series по строкам:")
+print(concat_s)
+
+# Объединение Series по столбцам (axis=1)
+concat_s_axis1 = pd.concat([s1, s2], axis=1)
+print("\nОбъединение Series по столбцам:")
+print(concat_s_axis1)
+
+# Преобразование Series в DataFrame
+df1 = s1.reset_index()
+df1.columns = ['Key', 'Value1']
+
+df2 = s2.reset_index()
+df2.columns = ['Key', 'Value2']
+
+# Объединение DataFrame с использованием merge
+merged_series_df = pd.merge(df1, df2, on='Key', how='outer')
+print("\nОбъединение Series после преобразования в DataFrame:")
+print(merged_series_df)
+
